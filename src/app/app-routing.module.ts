5621b5425dd9852f1a4b39582e191ff2ba30601f
Antonio Ramar Collins II <ramar@ironhead.io>
2022-08-19 14:19:26 -0400

    Started re-committing with modules in mind.
    I knew we would have to refactor, but not so soon.
    
    This commit marks the first of splitting out Garbanzo from a really simple SPA to a multipage Angular app.
    
    This also only includes the basics of the login module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'budget', loadChildren: () => import('./budget/budget.module').then(m => m.BudgetModule) }, 
+	{ path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+	{ path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
